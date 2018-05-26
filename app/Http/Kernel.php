ad2afc05a87533b32bacd5b5bcf6fa2d2331945c
Frédéric BAUCHER <fred@baucher.net>
2018-05-26 10:41:27 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,5 +48,6 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'adminer' => \App\Http\Middleware\Authenticate::class,        
     ];
 }
